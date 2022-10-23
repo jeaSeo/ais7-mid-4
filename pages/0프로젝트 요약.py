8c418cab2d747bc0bf9ccfad5ba6a0bddad22789
jeaSeo <104315524+jeaSeo@users.noreply.github.com>
2022-10-24 01:49:54 +0900

    Add files via upload
    취업관련 테이블 수정

--- pages/0프로젝트 요약.py
+++ pages/0프로젝트 요약.py
@@ -685,21 +685,38 @@ raw03 = load_data(url3)
 
 
 
+# 정규화 과정
+df_num = raw03[["실업률 (%)", "고용률 (%)"]]
+df_std = (df_num - df_num.mean())/df_num.std()
+df_std.insert(0, "시점", raw03["시점"])
+df_std["전년대비 물가상승률"] = raw03["전년대비 물가상승률"]
+
 st.markdown('## 전년대비 물가상승률과 연도별 고용률')
 fig1 = make_subplots(specs=[[{"secondary_y": True}]])
 
-fig1.add_trace(
-    go.Scatter(mode = 'lines+markers+text'
-            , x = raw03["시점"], y = raw03["실업률 (%)"]
-            , name = "연도별 실업률(%)", marker = dict(color = "#807DBA"))
+# 물가상승률과 실업률 비교
+st.markdown('#### 연도별 물가상승률과 취업률 및 실업률')
+# 연도별 물가상승률과 취업률 및 실업률
+fig15 = make_subplots(specs=[[{"secondary_y": True}]])
+
+fig15.add_trace(
+    go.Scatter(mode = 'lines+markers+text', x= df_std["시점"], y = df_std["전년대비 물가상승률"], 
+           name = "전년대비 물가상승률", marker = dict(color = "#DC5373")), secondary_y = False
 )
 
-fig1.add_trace(
-    go.Scatter(mode = 'lines+markers+text'
-               , x= raw03["시점"], y = raw03["고용률 (%)"]
-               , name = "연도별 고용률(%)", marker = dict(color = "#3F007D")), secondary_y = True)
+# 실업률(정규화)
+fig15.add_trace(
+    go.Scatter(mode = 'lines+markers+text', x = df_std["시점"], y = df_std["실업률 (%)"]
+           , name = "실업률", marker = dict(color = "#807DBA")),
+            secondary_y = True)
 
-fig1.update_layout(
+# 고용률(정규화)
+fig15.add_trace(
+    go.Scatter(mode = 'lines+markers+text', x = df_std["시점"], y = df_std["고용률 (%)"]
+           , name = "취업률", marker = dict(color = "#3F007D")),
+            secondary_y = True)
+
+fig15.update_layout(
 	legend=dict(
         orientation="h", # 가로 방향으로
         yanchor="top", y=1.15, # y축 방향 위치 설정
@@ -717,9 +734,10 @@ fig1.update_layout(
     showgrid=True
     , gridwidth=1
     , gridcolor='#f0f0f0'
-    , title_text='전년대비 물가상승률(%)'
+    , title_text="전년대비 물가상승률"
+    , secondary_y=False
 ).update_yaxes(
-    secondary_y = True
-    , title_text='연도별 고용률(%)'
+    title_text="취업률 및 고용률(%)"
+    , secondary_y=True
 )
-fig1
+fig15
